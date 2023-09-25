8926a77742762e491b84fac93f73f2ffd5ade051
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-09-25 13:50:45 +0200

    Changes in the format

--- untitled/src/main/java/Ejercicio1.java
+++ untitled/src/main/java/Ejercicio1.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        int base;
+        int height;
+        double result;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Escribe la base en centimetros:");
+        base=sc.nextInt();
+        System.out.println("Escribe la altura en centimetros");
+        height= sc.nextInt();
+        result=base*height;
+        System.out.println("El resultado es: "+result);
+    }
 }
