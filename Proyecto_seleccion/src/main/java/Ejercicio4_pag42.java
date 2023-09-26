e1d090bf3624cf590593fa50d907224b66c2a837
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-09-26 11:23:19 +0200

    Execise 4 incomplete

---
+++ Proyecto_seleccion/src/main/java/Ejercicio4_pag42.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Ejercicio4_pag42 {
+    public static int sumWorkHour(int workHour){
+        int totalHours;
+        int ordinaryHour= 12;
+
+        totalHours = ordinaryHour * workHour;
+        return totalHours;
+    }
+    public static int sumExtraHours(int workHour){
+        int extraHour= 16;
+        int totalHours;
+        int extraHoursWorked;
+        int extraHoursTotal;
+
+        extraHoursWorked = workHour-40;
+        extraHoursTotal = extraHoursWorked * extraHour;
+        totalHours =  + extraHoursTotal;
+
+        if (workHour>40){
+
+        }
+        return totalHours;
+    }
+    public static void main(String[] args) {
+        int workHour;
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Escribe las horas trabajadas esta semana:");
+        workHour=sc.nextInt();
+
+        if (workHour<=40){
+            System.out.println("Te corresponden "+sumWorkHour(workHour)+" Euros");
+        } else if (workHour>40) {
+            System.out.println("Te corresponden "+sumExtraHours(workHour)+" Euros");
+        }
+    }
+}
