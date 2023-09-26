40f41a759b9b7ec04383026e99d0fcd893ef4298
Jose Luis <condegomesjoseluis@gmail.com>
2023-09-26 23:32:59 +0200

    Exercise 5 complete.

---
+++ Proyecto_seleccion/src/main/java/Ejercicio5_pag42.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio5_pag42 {
+    public static int equationSolution(int a, int b) {
+        int x;
+        x= -(b/a);
+        return x;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int coefficient; //a
+        int constant; //b
+
+        System.out.println("Escribe el coeficiente:");
+        coefficient = sc.nextInt();
+        System.out.println("Escribe la constante");
+        constant=sc.nextInt();
+        System.out.println("La solucion es: "+equationSolution(coefficient,constant));
+    }
+}
