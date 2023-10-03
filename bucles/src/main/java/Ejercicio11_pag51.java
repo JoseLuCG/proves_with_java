b681eacc36987715bf20b1ead861a76fb5437a21
Jose Luis <condegomesjoseluis@gmail.com>
2023-10-04 00:13:22 +0200

    Ex11 solved.

--- bucles/src/main/java/Ejercicio11_pag51.java
+++ bucles/src/main/java/Ejercicio11_pag51.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Ejercicio11_pag51 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        // ----- not solved -----
         int number;
 
         System.out.println("Escribe tu numero: ");
         number= sc.nextInt();
 
         for (int i = 0; i<5; i++){
-            System.out.println(number+"     "+(number*number)+"     "+(number*number*number));
+            System.out.println((number+i)+"     "+((number+i)*(number+i))+"     "+((number+i)*(number+i)*(number+i)));
         }
 
     }
