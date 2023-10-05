8934e5c6039c9e737801541337157784a5c1d5d5
Jose Luis <condegomesjoseluis@gmail.com>
2023-10-05 17:11:39 +0200

    Ex14 Solved

--- bucles/src/main/java/Ejercicio14_pag51.java
+++ bucles/src/main/java/Ejercicio14_pag51.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Ejercicio14_pag51 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number, exponent, result=0;
+
+        System.out.println("Escribe el numero.");
+        number = sc.nextInt();
+        System.out.println("Escribe el exponente");
+        exponent = sc.nextInt();
+
+        for (int i=0; i<exponent; i++){
+            result=number*number;
+            result= result*number;
+        }
+        System.out.println("El resultado es: "+result);
+    }
 }
