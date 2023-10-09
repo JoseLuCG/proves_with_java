3db7c2e90c02829a5a02ab73100f2d1775e2a3aa
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-09 12:05:13 +0200

    Bad commit

--- bucles/src/main/java/Ejercicio14_pag51.java
+++ bucles/src/main/java/Ejercicio14_pag51.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Ejercicio14_pag51 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int base, exponent;
+        System.out.println("Dime la base.");
+        base = sc.nextInt()
+        System.out.println("Dime la potencia");
+        exponent = sc.nextInt();
+
+        for (int i=0; i< exponent; i++) {
+            base*base;
+        }
+    }
 }
