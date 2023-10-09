b43636f9ef0523ba7d9240ef94d305fa17e6f07d
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-09 14:19:21 +0200

    Ex17 complete.

--- bucles/src/main/java/Ejercicio17_pag51.java
+++ bucles/src/main/java/Ejercicio17_pag51.java
@@ -4,8 +4,16 @@ public class Ejercicio17_pag51 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number;
+        int suma=0;
 
-        System.out.println("Escribe un numero:");
+        do {
+            System.out.println("Escribe un numero:");
+            number = sc.nextInt();
+        } while (number<0);
 
+        for (int i = number; i< number + 100;i++){
+            suma +=i;
+            System.out.println( number+"+"+suma+"="+(number+suma) );
+        }
     }
 }
