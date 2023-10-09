03adc72a82158a793630a1b39cd328e310244553
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-09 12:33:29 +0200

    Ex15 Solved.

---
+++ bucles/src/main/java/Ejercicio15_pag51.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio15_pag51 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int base, exponent;
+
+        System.out.println("Escribe un numero real como base:");
+        base = sc.nextInt();
+        System.out.println("Escribe el exponente:");
+        exponent= sc.nextInt();
+
+        for (int i=0;i<exponent; i++) {
+            System.out.println(base+"^"+(exponent-i));
+        }
+    }
+}
