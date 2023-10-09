7a59c74db63587d09d972eb4f64b4c27d6295b4f
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-09 14:50:25 +0200

    Ex18 completed.

--- bucles/src/main/java/Ejercicio18_pag51.java
+++ bucles/src/main/java/Ejercicio18_pag51.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class Ejercicio18_pag51 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int firstNumber, secondNumber;
+
+
+        do {
+            System.out.println("Escribe un numero.");
+            firstNumber = sc.nextInt();
+            System.out.println("Escribe el segundo numero.");
+            secondNumber = sc.nextInt();
+
+            if (firstNumber == secondNumber){
+                System.out.println("los numeros son iguales. Deben ser distintos.");
+            }
+
+        } while (firstNumber == secondNumber);
+
+        if (secondNumber<firstNumber) {
+            int temp = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = temp;
+        }
+        for (int i = firstNumber; i<secondNumber; i+=7){
+            System.out.print(i + " ");
+        }
+    }
 }
