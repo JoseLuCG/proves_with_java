7c5a910467f570b95ee06f80a8251e5cf2b9636c
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-11 10:25:31 +0200

    Add Ex25.

---
+++ bucles/src/main/java/Ejercicio25_pag51.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ejercicio25_pag51 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+
+        System.out.println("Escribe un número:");
+
+    }
+}
