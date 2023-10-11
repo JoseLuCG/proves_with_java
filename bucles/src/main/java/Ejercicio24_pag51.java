9d9098407c30977edff0ecebab8c6d1336e4fae8
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-11 09:59:13 +0200

    Complete Exercise 24.

--- bucles/src/main/java/Ejercicio24_pag51.java
+++ bucles/src/main/java/Ejercicio24_pag51.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+
 public class Ejercicio24_pag51 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int height = 1;
+        int userHeight;
+
+        System.out.println("Introduce una altura:");
+        userHeight= sc.nextInt();
+
+        int spaces= userHeight-1;
+
+        while (height<=userHeight){
+
+            //Loop that prints spaces:
+            for (int spacePrint=1; spacePrint<=spaces; spacePrint++){
+                System.out.print(" ");
+            }
+
+            //Loop that print the numbres line:
+            for (int numPrint=1; numPrint<height;numPrint++){
+                System.out.print(numPrint);
+            }
+
+            //Loop that reverse the numbers.
+            for ( int numReverse = height; numReverse>0; numReverse--){
+                System.out.print(numReverse);
+            }
+            System.out.println();
+            height++;
+            spaces--;
+        }
+    }
 }
