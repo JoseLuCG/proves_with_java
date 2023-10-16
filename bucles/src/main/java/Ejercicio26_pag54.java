5701637e55d44649193826ce7279be719f3b38a6
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-16 14:19:27 +0200

    Add ex26

---
+++ bucles/src/main/java/Ejercicio26_pag54.java
@@ -0,0 +1,5 @@
+public class Ejercicio26_pag54 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ bucles/src/main/java/paralelogramo.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class paralelogramo {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int height, width;
+
+        System.out.println("Escribe la altura de la figura:");
+        height = sc.nextInt();
+        System.out.println("Escribe la base");
+        width = sc.nextInt();
+        System.out.println("Dime un simbolo");
+        String p = sc.next();
+
+        for (int row=0; row<height; row++){
+            //Loop that print the spaces
+            for ( int spaces=0; spaces < height - row;spaces++){
+                System.out.print("  ");
+            }
+            // Loop that print the character:
+            for (int column = 0; column<width; column++){
+                if (row == 0 || row == height - 1){
+                    System.out.print(p+" ");
+                } else if ( column == 0 || column == width -1) {
+                    System.out.print(p+" ");
+                }else System.out.print("  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
