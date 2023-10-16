acdfaec75ddc03fcdfe99a5d37219800606da7dd
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-16 13:10:38 +0200

    Exercise 25 Solved

--- bucles/src/main/java/Ejercicio25_pag51.java
+++ bucles/src/main/java/Ejercicio25_pag51.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class Ejercicio25_pag51 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number;
+        int number, counter=0, r;
+        int inv=0;
 
         System.out.println("Escribe un número:");
+        number =sc.nextInt();
 
+        while ( number!=0 ){
+            counter++;
+            r=  number%10;
+            number=number/10;
+            inv=inv*10+r;
+
+        }
+        System.out.println("El numero resultante es: "+ inv);
     }
 }
