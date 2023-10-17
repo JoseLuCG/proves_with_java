e7799dc1e6ad7a9571bb16f882af26a59d6e5f8f
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-17 10:38:44 +0200

    ex29

--- bucles/src/main/java/Ejercicio29_pag54.java
+++ bucles/src/main/java/Ejercicio29_pag54.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+/*
+    Pendiente de acabar
+ */
 public class Ejercicio29_pag54 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
