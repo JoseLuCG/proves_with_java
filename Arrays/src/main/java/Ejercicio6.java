55dda7511b9a0d2def2bf2aeb68c63c28cda84d0
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-10-25 10:29:29 +0200

    Add exercise 6, not complete.

---
+++ Arrays/src/main/java/Ejercicio6.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[15];
+        int temp;
+
+        System.out.println("Escribe 15 numeros:");
+        for (int idx=0; idx<15; idx++){
+            numbers[idx]= sc.nextInt();
+        }
+        System.out.println(Arrays.toString(numbers));
+
+        for (int idx=0; idx<15; idx++){
+            numbers
+        }
+    }
+}
