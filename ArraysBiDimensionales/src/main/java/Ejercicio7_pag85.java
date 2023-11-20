6fbfb67a9fe7ab8f8afd178e6d3715c9022913ab
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-11-20 12:38:18 +0100

    Add ex7

--- ArraysBiDimensionales/src/main/java/Ejercicio7_pag85.java
+++ ArraysBiDimensionales/src/main/java/Ejercicio7_pag85.java
@@ -8,8 +8,8 @@ public class Ejercicio7_pag85 {
         int[][] juego = new int[5][4];
         int f= rnd.nextInt(0,5);
         int c= rnd.nextInt(0,4);
-        int fm, cm;
-        /**
+        int fm, cm, cont=0;
+        /*
          * Tesoro = 2
          * Mina = 1
          */
@@ -24,13 +24,17 @@ public class Ejercicio7_pag85 {
         }while (f==fm && c==cm);
         //Agignamos la mina:
         juego[fm][cm] = 1;
+
         // Vendria el juego
         do {
-            System.out.println("Introduce las coordenadas donde crees que esta el tesoro.");
+
             do {
+                System.out.println("Introduce las coordenadas donde crees que esta el tesoro. Entre 1-5 y de 1-4");
                 f = sc.nextInt();
                 c = sc.nextInt();
+            }while (f>5 ||c>4);
+            f--; c--; cont++;
 
-        }while (juego[f][c] != 1 || juego[f][c]!= 2);
+        }while ( ( juego[f][c] != 1 || juego[f][c] != 2 ) && cont<3 );
     }
 }
