e5b3d2280f8f9c5973af2849fef87f44983185e2
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-11-27 14:50:40 +0100

    Add code.

--- Juego3Raya/src/main/java/juego_3_en_raya.java
+++ Juego3Raya/src/main/java/juego_3_en_raya.java
@@ -1,12 +1,103 @@
 import java.util.Scanner;
 
 public class juego_3_en_raya {
+    /**
+     * Funcion que recorre la matriz fila a fila para el jugador 1.
+     * @param matriz array a recorrer.
+     * @param victoryCondition condición que para el bucle.
+     * @return - Devuelve la condición que permite que el jeugo se siga ejecutando.
+     */
+    public static boolean recorridoLineaJ1 (char[][] matriz, boolean victoryCondition){
+        int winXCounter=0;
+        for (int f=0; f< matriz.length;f++){
+            winXCounter=0;
+            for (int c=0; c< matriz[0].length; c++){
+                if (matriz[f][c] == 'x'){
+                    winXCounter++;
+                    if (winXCounter == 3) {
+                        victoryCondition= true;
+                        System.out.println("Jugaor 1, has ganado la partida.");
+                    }
+                }
+            }
+        }
+        return victoryCondition;
+    }
+
+    /**
+     * Funcion que recorre la matriz fila a fila para el jugador 2.
+     * @param matriz array a recorrer.
+     * @param victoryCondition condición que para el bucle.
+     * @return - Devuelve la condición que permite que el jeugo se siga ejecutando.
+     */
+    public static boolean recorridooLineaJ2 (char[][] matriz, boolean victoryCondition){
+        int winOCounter;
+        for (int f=0; f< matriz.length;f++){
+            winOCounter=0;
+            for (int c=0; c< matriz[0].length; c++){
+                if (matriz[f][c] == 'o'){
+                    winOCounter++;
+                    if (winOCounter == 3) {
+                        victoryCondition= true;
+                        System.out.println("Jugaor 2, has ganado la partida.");
+                    }
+                }
+            }
+        }
+        return victoryCondition;
+    }
+
+    /**
+     * Funcion que recorre la matriz columna a columna para el jugador 1.
+     * @param matriz array a recorrer.
+     * @param victoryCondition condición que para el bucle.
+     * @return - Devuelve la condición que permite que el jeugo se siga ejecutando.
+     */
+    public static boolean recorridoColumnaJ1 (char[][] matriz, boolean victoryCondition){
+        int winXCounter;
+        for (int c=0; c< matriz[0].length;c++){
+            // Recorremos las columnas una por una para saber si hay una linea de 3.
+            winXCounter=0;
+            for (int f=0; f< matriz.length; f++){
+                if (matriz[f][c] == 'x'){
+                    winXCounter++;
+                    if (winXCounter == 3) {
+                        victoryCondition= true;
+                        System.out.println("Jugaor 1, has ganado la partida.");
+                    }
+                }
+            }
+        }
+        return victoryCondition;
+    }
+    /**
+     * Funcion que recorre la matriz columna a columna para el jugador 2.
+     * @param matriz array a recorrer.
+     * @param victoryCondition condición que para el bucle.
+     * @return - Devuelve la condición que permite que el jeugo se siga ejecutando.
+     */
+    public static boolean recorridoColumnaJ2 (char[][] matriz, boolean victoryCondition){
+        int winOCounter;
+        for (int c=0; c< matriz[0].length;c++){
+            winOCounter=0;
+            for (int f=0; f< matriz.length; f++){
+                if (matriz[f][c] == 'o'){
+                    winOCounter++;
+                    if (winOCounter == 3) {
+                        victoryCondition= true;
+                        System.out.println("Jugaor 2, has ganado la partida.");
+                    }
+                }
+            }
+        }
+        return victoryCondition;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char[][] board = new char[3][3];
         int rowPosition, columnPosition;
         boolean finish = false;
-        int playerCount=1, winXCounter=0, winOCounter=0, dwinOCounter=0;
+        int playerCount=1, winOCounter=0, dwinOCounter=0;
         String jugagor1 ="Jugador 1. ";
         String jugador2 ="Jugador 2. ";
         String player="Jugador 1. ";
@@ -26,9 +117,9 @@ public class juego_3_en_raya {
             }
 
             // ---------- Preguntamos en que posicion se colocará la ficha. ----------
-            System.out.println(player+"Indica la posicion de ls fila:");
+            System.out.println(player+"Indica la posicion de la fila:");
             rowPosition = sc.nextInt();
-            System.out.println(player+"Indica la posicion de la coolumna");
+            System.out.println(player+"Indica la posicion de la columna");
             columnPosition = sc.nextInt();
 
             //---------- Asignamos la marca en la posición ----------
@@ -44,62 +135,14 @@ public class juego_3_en_raya {
             }
             //---------- Condiciones de victoria. ----------
             //---------- Recorrido de filas ----------
-            //----- Recorrido linea a linea para jugador 1: -----
-            for (int f=0; f< board.length;f++){
-                // Recorremos las filas una por una para saber si hay una linea de 3.
-                winXCounter=0;
-                for (int c=0; c< board[0].length; c++){
-                    if (board[f][c] == 'x'){
-                        winXCounter++;
-                        if (winXCounter == 3) {
-                            finish= true;
-                            System.out.println("Jugaor 1, has ganado la partida.");
-                        }
-                    }
-                }
-            }
+            recorridoLineaJ1(board, finish);
             //----- Recorrido linea a linea para jugador 2: -----
-            for (int f=0; f< board.length;f++){
-                winOCounter=0;
-                for (int c=0; c< board[0].length; c++){
-                    if (board[f][c] == 'o'){
-                        winOCounter++;
-                        if (winOCounter == 3) {
-                            finish= true;
-                            System.out.println("Jugaor 2, has ganado la partida.");
-                        }
-                    }
-                }
-            }
+            recorridooLineaJ2(board, finish);
             //---------- Recorrido de columnas: ----------
             //----- Recorrido en columnas para el jugador 1
-            for (int c=0; c< board[0].length;c++){
-                // Recorremos las columnas una por una para saber si hay una linea de 3.
-                winXCounter=0;
-                for (int f=0; f< board.length; f++){
-                    if (board[f][c] == 'x'){
-                        winXCounter++;
-                        if (winXCounter == 3) {
-                            finish= true;
-                            System.out.println("Jugaor 1, has ganado la partida.");
-                        }
-                    }
-                }
-            }
-
+            recorridoColumnaJ1(board, finish);
             //----- Recorrido en columnas para el jugador 2
-            for (int c=0; c< board[0].length;c++){
-                winOCounter=0;
-                for (int f=0; f< board.length; f++){
-                    if (board[f][c] == 'o'){
-                        winOCounter++;
-                        if (winOCounter == 3) {
-                            finish= true;
-                            System.out.println("Jugaor 2, has ganado la partida.");
-                        }
-                    }
-                }
-            }
+            recorridoColumnaJ2(board, finish);
             //---------- Recorrido diagonal ----------
             //----- Recorrido en diagonal principal para el jugador 1
             for (int p=0; p< board.length; p++){
@@ -128,7 +171,7 @@ public class juego_3_en_raya {
             for (int p=0; p< board.length; p++){
                 if ( board[p][c]=='o'){
                     dwinOCounter++;
-                    if (winOCounter == 3) {
+                    if (dwinOCounter == 3) {
                         finish= true;
                         System.out.println("Jugaor 2, has ganado la partida.");
                     }
