96555daffe81e29b61b632bfcff934e3f3d059d2
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-11-28 12:11:11 +0100

    Ex8.

--- ArraysBiDimensionales/src/main/java/EjerciciosProfesora/Ejercicio8_ejerciciosMatrices.java
+++ ArraysBiDimensionales/src/main/java/EjerciciosProfesora/Ejercicio8_ejerciciosMatrices.java
@@ -1,2 +1,16 @@
-package EjerciciosProfesora;public class Ejercicio8_ejerciciosMatrices {
+package EjerciciosProfesora;
+
+import java.util.Random;
+
+public class Ejercicio8_ejerciciosMatrices {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int[][] matrix = new int[4][4];
+
+        for (int f = 0; f < matrix.length; f++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+
+            }
+        }
+    }
 }
