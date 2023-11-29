325da35a119b4b8049e890a0ef5b9462ae5c9ab4
Jose Luis <condegomesjoseluis@gmail.com>
2023-11-29 08:17:21 +0100

    add ex8.

--- ArraysBiDimensionales/src/main/java/EjerciciosProfesora/Ejercicio8_ejerciciosMatrices.java
+++ ArraysBiDimensionales/src/main/java/EjerciciosProfesora/Ejercicio8_ejerciciosMatrices.java
@@ -3,14 +3,24 @@ package EjerciciosProfesora;
 import java.util.Random;
 
 public class Ejercicio8_ejerciciosMatrices {
+    static Random rnd = new Random();
+    public static int randomNumber() {
+        int positionRandom = rnd.nextInt(1,17);
+        return positionRandom;
+    }
     public static void main(String[] args) {
-        Random rnd = new Random();
         int[][] matrix = new int[4][4];
+        int couple=0;
 
-        for (int f = 0; f < matrix.length; f++) {
-            for (int c = 0; c < matrix[0].length; c++) {
-
+        int c = matrix[0].length;
+        for (int p = 1; p<=8; p++) {
+            int rN = randomNumber();
+            if (matrix[rN/c][rN%c] == 0){
+                matrix[rN/c][rN%c] = p;
+                couple++;
+                int pareja = (couple<2) ? p-- : couple=0;
             }
         }
     }
 }
+
