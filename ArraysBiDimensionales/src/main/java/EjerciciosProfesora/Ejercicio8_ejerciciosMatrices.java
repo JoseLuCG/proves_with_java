44a66ac74250328824290bfb17eaf462e4d7d54e
unknown <DAW106@acarballeira.local>
2023-11-29 09:16:17 +0100

    Complete the exercise 8.

--- ArraysBiDimensionales/src/main/java/EjerciciosProfesora/Ejercicio8_ejerciciosMatrices.java
+++ ArraysBiDimensionales/src/main/java/EjerciciosProfesora/Ejercicio8_ejerciciosMatrices.java
@@ -5,22 +5,36 @@ import java.util.Random;
 public class Ejercicio8_ejerciciosMatrices {
     static Random rnd = new Random();
     public static int randomNumber() {
-        int positionRandom = rnd.nextInt(1,17);
+        int positionRandom = rnd.nextInt(0,16);
         return positionRandom;
     }
-    public static void main(String[] args) {
-        int[][] matrix = new int[4][4];
+    public static void stuffedMatrix (int [][] matrix){
+        int c = matrix[0].length;
         int couple=0;
 
-        int c = matrix[0].length;
         for (int p = 1; p<=8; p++) {
             int rN = randomNumber();
             if (matrix[rN/c][rN%c] == 0){
                 matrix[rN/c][rN%c] = p;
                 couple++;
-                int pareja = (couple<2) ? p-- : couple=0;
-            }
+                if (couple<2) { p--;
+                }else { couple=0;}
+            }else p--;
         }
+    }
+    public static void printMatrix ( int[][] matrix){
+        for (int f = 0; f < matrix.length; f++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                System.out.printf("%3d",matrix[f][c]);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int[][] matrix = new int[4][4];
+
+        stuffedMatrix(matrix);
+        printMatrix(matrix);
     }
 }
 
