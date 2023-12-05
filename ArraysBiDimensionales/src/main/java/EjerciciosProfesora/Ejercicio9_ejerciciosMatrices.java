09b4e90b687a16c997d0591466f90b56295cfbfa
unknown <daw106@ACARBALLEIRA.LOCAL>
2023-12-05 09:25:06 +0100

    Complete the code.

--- ArraysBiDimensionales/src/main/java/EjerciciosProfesora/Ejercicio9_ejerciciosMatrices.java
+++ ArraysBiDimensionales/src/main/java/EjerciciosProfesora/Ejercicio9_ejerciciosMatrices.java
@@ -1,25 +1,25 @@
 package EjerciciosProfesora;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Ejercicio9_ejerciciosMatrices {
     public static final String RED = "\u001B[31m";
     public static final String WHITE = "\u001B[37m";
-    public static final String RESET= "\033[39m";
+    public static final String RESET = "\033[39m";
     public static final String GREEN = "\u001B[32m";
     public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
     /**
      * This fuction fill the matrix with random numbers.
      * @param matrix - A array for fill in.
      */
-    public static void fillMatrix (int[][] matrix) {
+    public static void fillMatrix(int[][] matrix) {
         Random rnd = new Random();
         int randomNumber;
 
-        for (int f=0; f< matrix.length; f++){
+        for (int f = 0; f < matrix.length; f++) {
             for (int c = 0; c < matrix[0].length; c++) {
-                randomNumber  = rnd.nextInt(0, 501);
+                randomNumber = rnd.nextInt(0, 501);
                 matrix[f][c] = randomNumber;
             }
         }
@@ -29,34 +29,39 @@ public class Ejercicio9_ejerciciosMatrices {
      * Show the matrix for console.
      * @param matrix - Array to show.
      */
-    public static void showMatrix (int[][] matrix, int[] sumFila, int[] sumCol, int[] bProduct){
-        String[] mounth = {"Enero","Febrero","Marzo","Abril","Mayo","Junio",
-                            "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+    public static void showMatrix(int[][] matrix, int[] sumFila, int[] sumCol, int[] bProduct, int[] data) {
+        String[] month = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        int monthIndex = data[1];
+        String monthBestSell = month[monthIndex];
 
-        System.out.printf("MESES %3s","");
-        for (int m = 0; m < mounth.length; m++) {
-            System.out.printf("%11s |",mounth[m]);
+        System.out.printf("MESES %3s", "");
+        for (int m = 0; m < month.length; m++) {
+            System.out.printf("%11s |", month[m]);
         }
         System.out.print("       Total");
         System.out.println();
-        for (int f=0; f< matrix.length; f++){
-            System.out.printf(PURPLE+"prdt %2d |",(f+1));
+        for (int f = 0; f < matrix.length; f++) {
+            System.out.printf(PURPLE + "prdt %2d |", (f + 1));
             for (int c = 0; c < matrix[0].length; c++) {
-                System.out.printf(WHITE+"%11d |",matrix[f][c]);
+                System.out.printf(WHITE + "%11d |", matrix[f][c]);
             }
-            System.out.printf(RED+"%12d |",sumFila[f]);
+            System.out.printf(RED + "%12d |", sumFila[f]);
             System.out.println();
         }
-        System.out.printf("TOTAL %3s","");
-        for (int p=0; p< sumCol.length; p++){
-            System.out.printf(GREEN+"%11d |",sumCol[p]);
+        System.out.printf("TOTAL %3s", "");
+        for (int p = 0; p < sumCol.length; p++) {
+            System.out.printf(GREEN + "%11d |", sumCol[p]);
+        }
+        System.out.println(RESET);
+        System.out.println("La cantidad mas alta de los productos vendidos son:");
+        System.out.printf("CantMax %1s", "");
+        for (int p = 0; p < bProduct.length; p++) {
+            System.out.printf(CYAN + "%11d |", bProduct[p]);
         }
         System.out.println();
-        System.out.println("La cantidad mas alta de los productos vendidos son:");
-        System.out.printf("CantMax %1s","");
-        for (int p=0; p< bProduct.length; p++){
-            System.out.printf(RED+"%11d |",bProduct[p]);
-        }
+        System.out.println(RESET+"Las mejores ventas son:");
+        System.out.println("El producto mas vendido es el producto " +(data[0]+1)+", del mes: "+monthBestSell+" en la cantidad de: "+data[2]);
     }
 
     /**
@@ -64,7 +69,7 @@ public class Ejercicio9_ejerciciosMatrices {
      * @param matrix - Matrix to operate.
      * @return - A array with the data of the sums.
      */
-    public static int[] sumSalesForProductInYear (int [][] matrix) {
+    public static int[] sumSalesForProductInYear(int[][] matrix) {
         int[] sumMouthRows = new int[50];
         int sumator;
 
@@ -83,12 +88,12 @@ public class Ejercicio9_ejerciciosMatrices {
      * @param matrix - Matrix to operate.
      * @return - A array with the data of the sums.
      */
-    public static int[] sumSalesForMouth (int[][] matrix){
+    public static int[] sumSalesForMouth(int[][] matrix) {
         int[] sumColMouth = new int[12];
         int sumator;
 
         for (int c = 0; c < matrix[0].length; c++) {
-            sumator=0;
+            sumator = 0;
             for (int f = 0; f < matrix.length; f++) {
                 sumator = sumator + matrix[f][c];
             }
@@ -102,14 +107,14 @@ public class Ejercicio9_ejerciciosMatrices {
      * Show the best selling product of the each mouth.
      * @param matrix - Matrix for operate.
      */
-    public static int[] showbestSellingProduct (int[][] matrix) {
+    public static int[] showbestSellingProduct(int[][] matrix) {
         int max;
         int[] bestSellingProduct = new int[12];
 
         for (int c = 0; c < matrix[0].length; c++) {
             max = Integer.MIN_VALUE;
             for (int f = 0; f < matrix.length; f++) {
-                if (matrix[f][c] > max){
+                if (matrix[f][c] > max) {
                     max = matrix[f][c];
                 }
             }
@@ -117,19 +122,38 @@ public class Ejercicio9_ejerciciosMatrices {
         }
         return bestSellingProduct;
     }
+    /**
+     * Calculate the best selling product of the table.
+     * @param matrix - Matrix for operate.
+     */
+    public static int[] bestSellingForMonth (int[][] matrix) {
+        int[] data = new int[3];
+        int maxValue = Integer.MIN_VALUE;
+
+        for (int f = 0; f < matrix.length; f++) {
+            for (int c = 0; c < matrix[0].length; c++) {
+                if (matrix[f][c] > maxValue) {
+                    maxValue = matrix[f][c];
+                    data[2] = maxValue;
+                    data[0] = f;
+                    data[1] = c;
+                }
+            }
+        }
+        return data;
+    }
 
     public static void main(String[] args) {
         int[][] ventas = new int[50][12];
-        int[] sumRows, sumCols, bestProducts;
+        int[] sumRows, sumCols, bestProducts, salesForMonth;
 
         fillMatrix(ventas);
-
 
         sumRows = sumSalesForProductInYear(ventas);
         sumCols = sumSalesForMouth(ventas);
         bestProducts = showbestSellingProduct(ventas);
+        salesForMonth = bestSellingForMonth(ventas);
 
-
-        showMatrix(ventas,sumRows,sumCols,bestProducts);
+        showMatrix(ventas, sumRows, sumCols, bestProducts, salesForMonth);
     }
 }
